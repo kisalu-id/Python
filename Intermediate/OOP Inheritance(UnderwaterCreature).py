7d741f2b318a11e33619a7cdec333971d55a6b22
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-06-09 22:05:49 +0200

    Update and rename Inheritance(UnderwaterCreature).py to OOP Inheritance(UnderwaterCreature).py

--- Intermediate/OOP Inheritance(UnderwaterCreature).py
+++ Intermediate/OOP Inheritance(UnderwaterCreature).py
@@ -9,16 +9,16 @@ class UnderwaterCreature:
 
 class Orca(UnderwaterCreature):
     def __init__(self, name, habitat, lenght):
-        super(), __init__(name, habitat) #common attributes name and habitat, common method describe
+        super().__init__(name, habitat) #common attributes name and habitat, common method describe
         self.lenght = lenght
 
-        def describe(self):
-            return f"{self.name} is an Orca that is {self.length} meters long and lives in the {self.habitat}. They are known for their intelligence and complex social structure."
+    def describe(self):
+        return f"{self.name} is an Orca that is {self.lenght} meters long and lives in the {self.habitat}. They are known for their intelligence and complex social structure."
 
-
+  
 class Clownfish(UnderwaterCreature): #each subclass inherits using the super() funct, but adds unique attr, each subclass ovverrides describe method
     def __init__(self, name, habitat, species):
-        super(), __init__(name, habitat) #common attributes name and habitat, common method describe
+        super().__init__(name, habitat) #common attributes name and habitat, common method describe
         self.species = species
 
     def describe(self):
@@ -27,26 +27,31 @@ class Clownfish(UnderwaterCreature): #each subclass inherits using the super() f
 
 class Eel(UnderwaterCreature):
     def __init__(self, name, habitat, lenght, wrong_species,correct_species, electric):
-        super(), __init__(name, habitat) #is to call methods and access attributes from the parent class within a subclass
+        super().__init__(name, habitat) #is to call methods and access attributes from the parent class within a subclass
         self.electric = electric
         self.lenght = lenght
         self.wrong_species = wrong_species
         self.correct_species = correct_species
 
     def describe(self):
-        return f"{self.name} is a not {self.wrong_species}, but a {correct_species}. It lives in the {self.habitat}. It is {self.lenght} meters long and is {self.electric}."
+        return f"{self.name} is not a {self.wrong_species}, but a {self.correct_species}. It lives in the {self.habitat}. It is {self.lenght} meters long and it is {'electric' if self.electric else 'non-electric'}."
 
 
 class Shark(UnderwaterCreature):
     def __init__(self, name, habitat, lenght):
-        super(), __init__(name, habitat)
+        super().__init__(name, habitat)
         self.lenght = lenght
 
     def describe(self):
         return f"{self.name} is a viper shark that lives in the {self.habitat} and is {self.lenght} meters long."
 
 
-orca = Orca("Orcinus orca", )
+orca = Orca("Orcinus orca", "ocean", 9)
 nemo = Clownfish("Amphiprion ocellaris", "coral reefs of the Indian Ocean", "Clownfish")
-eel = Eel("Anguilla japonica", "eel", "knifefisch")
-shark = Shark("Electrophorus electricus", "Pacific Ocean")
+eel = Eel("Anguilla japonica", "river", 2, "knifefisch", "eel", True)
+shark = Shark("Trigonognathus kabeyai", "Pacific Ocean", 0.5)
+
+print(orca.describe())
+print(nemo.describe())
+print(eel.describe())
+print(shark.describe())
