6bd575cfbc9cfb6d3d84b6bb854035f04e0c5174
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-06-19 17:38:05 +0200

    Create Nesting_report.py

---
+++ Advanced/Nesting_report.py
@@ -0,0 +1,26 @@
+from company import dlg
+from sclcore import do_debug
+from config import run_config
+
+
+
+def main():
+    do_debug()
+    file_path = 
+    #get NestGetSheets()
+    nest_get_sheets = 
+    if nest_get_sheets:
+        with open(file_path, "r") as f:
+            sheets =
+            for sheet in sheets:
+                area = _NSheetArea
+                matherial = 
+                width
+                height
+                pieces =
+                curr1 =
+                curr2 =
+                total_area += area
+                total_left += (area * curr1) / 100
+
+                
