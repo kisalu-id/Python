b889467f9727f5876fc28d8caf249c897e8d30ec
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-06-23 21:47:45 +0200

    Update Nesting_report.py

--- Advanced/Nesting_report.py
+++ Advanced/Nesting_report.py
@@ -7,6 +7,8 @@ from config import run_config
 def main():
     do_debug()
     nFileReport = r'C:\Users\...\Nesting_report\outlog.txt'
+
+
     nest.activate()
     nest_get_sheets = nest.get_sheets()
 
@@ -23,34 +25,32 @@ def main():
                 matherial = nest.get_sheet_property(sheet, nest.SheetProperties.MATERIAL)        #_NSheetMaterial
                 width = nest.get_sheet_property(sheet, nest.SheetProperties.WIDTH)               #_NSheetWidth
                 height = nest.get_sheet_property(sheet, nest.SheetProperties.HEIGHT)             #_NSheetHeight
-                pieces = nest.get_sheet_property(sheet, nest.SheetProperties.PIECES_NUMBER)       #_NSheetNumPieces
+                pieces = nest.get_sheet_property(sheet, nest.SheetProperties.PIECES_NUMBER)      #_NSheetNumPieces
                 curr1 = nest.get_sheet_property(sheet, nest.SheetProperties.RATE_LEFT_OVER)      #_NSheetRateLeftOver  % of sheet   garbage not reusable material
                 curr2 = nest.get_sheet_property(sheet, nest.SheetProperties.RATE_REUSABLE)       #_NSheetRateReusable  % of sheet   reusable material
-                abfall = area * ( (curr1 + curr2) / 100) #total cut off mat
-                total_area += area
-                total_left += abfall
-                total_garbage += curr1
-                total_reusable += curr2
+                area = (area / 1000000) # m2
+                total_area += area      # m2
+                total_garbage += curr1  # %
+                total_reusable += curr2 # %
                 sC = str(nC).zfill(3)
                 nSheets += 1
                 nC += 1
 
-                line = f"Sheet{sC} - Material : {matherial}, Länge : {width}, Breite : {height}, Guteile : {pieces}\n"
-                file.write(line)
+                line_stats = f"Sheet{sC} - Material : {matherial}, Länge : {(width / 1000)}m, Breite : {(height / 1000)}m, Guteile : {pieces}\n"
+                file.write(line_stats)
 
-                linetest = f"Efficiency of Sheet{sC} : area {round(area, 2)}, not reusable material curr1 {round(curr1, 2)} (% of sheet),  reusable material curr2 {round(curr2, 2)} (% of sheet), cut off on total: abfall {round(abfall, 2)} mm2???\n\n"
-                file.write(linetest)
+                line_eff = f"Effizienz von Sheet{sC}:\n  Fläche:  {round(area, 2)} m²,\n  Nicht wiederverwendbares Material:  {round(curr1, 2)}% des Blattes,\n  Wiederverwendbares Material:        {round(curr2, 2)}% des Blattes\n\n"
+                
+                file.write(line_eff)
 
-            file.write(f"Anzahl Sheets : {nSheets}\n")
-            file.write(f"Gesamtfläche in m2 : {round(total_area, 2)}\n")
-            #file.write(f"Gesamtverschnitt in m2 : {total_left}\n")
-            #file.write(f"Prozent Verschnitt : {total_left / total_area * 100}\n")      #dont need that i think, its not saying the impoortant part
-            file.write(f"Total garbage not reusable material in m2 : {round(total_garbage / nSheets, 2)}\n"
-            file.write(f"Total reusable material in m2 : {round(total_reusable / nSheets, 2)}\n")
-
-            x = 5
-
-
+            total_garbage /= nSheets #bc I need to not only add % from every sheet but also get the Durchschnitt
+            total_reusable /= nSheets
+            file.write(f"\nAnzahl Sheets :  {nSheets}\n")
+            file.write(f"Gesamtfläche:  {round(total_area, 2)} m²\n\n")
+            file.write(f"Wiederverwendbares Material pro Blatt im Durchschnitt:        {round(total_area * total_reusable / 100, 2)} m²\n")
+            file.write(f"Nicht wiederverwendbares Material pro Blatt im Durchschnitt:  {round(total_area * total_garbage / 100, 2)} m²\n\n")
+            file.write(f"Wiederverwendbares Material pro Blatt im Durchschnitt:        {round(total_reusable, 2)}%\n")
+            file.write(f"Nicht wiederverwendbares Material pro Blatt im Durchschnitt:  {round(total_garbage, 2)}%\n\n")
 
 if __name__ == '__main__':
     main()
